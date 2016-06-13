2cf12dbaf33ca7b23f7958b87ec86e7a19cca92a
Ali <acherifi@etu.u-bordeaux.fr>
2016-06-13 10:11:39 +0200

    récupération au CREMI

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -11,28 +11,14 @@ public class Test {
 
     public static void main(String args[]) throws IOException {
 
-        YehSaliency carte = new YehSaliency();
+      
 
         XuggleVideo video = new XuggleVideo("src/main/resources/video.mp4");
+        
+        
 
-        int nbFrame = 0;
-        while (video.hasNextFrame()){
-
-            File dest = new File("saliency" + nbFrame +".png");
-            nbFrame +=1;
-
-            MBFImage frame = (MBFImage)video.getCurrentFrame();
-            FImage sal;
-
-            carte.analyseImage(frame);
-            System.out.print("ANALYSE FAITE");
-            sal = carte.getSaliencyMap();
-            System.out.print("CARTE FAITE");
-
-            ImageUtilities.write(sal,dest);
-
-            frame = video.getNextFrame();
+       
         }
 
     }
-}
+
