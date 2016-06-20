9129740ff1c3b47c574c088bb8055af47ca95dd8
Ali <ali.cherifi@etu.u-bordeaux.fr>
2016-06-20 21:03:51 +0200

    Correction de la clase de test

--- src/main/test/TestClass.java
+++ src/main/test/TestClass.java
@@ -1,17 +1,5 @@
-import org.junit.Assert;
-import org.junit.Test;
-
 public class TestClass {
 
-    @Test
-    public void addTest(){
-        Barcode tst = new Barcode();
-        int result = tst.add(5,5);
-        Assert.assertEquals(10,result);
-
-
-
-   }
 }
 
 
