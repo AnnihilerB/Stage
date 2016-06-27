8230ad7b9a1af20fd50373a39d56ae4d1bd5a698
Ali <ali.cherifi@etu.u-bordeaux.fr>
2016-06-27 17:01:51 +0200

    Début intrface graphique

--- src/main/java/interfacegraphique/Fenetre.java
+++ src/main/java/interfacegraphique/Fenetre.java
@@ -1,7 +1,29 @@
 package interfacegraphique;
 
-/**
- * Created by ali on 26/06/16.
- */
-public class Fenetre {
+import javax.swing.*;
+import java.awt.*;
+
+public class Fenetre extends JFrame{
+
+    JPanel conteneur;
+
+    public Fenetre(){
+        this.setTitle("Video 0.1");
+        this.setPreferredSize(new Dimension(800,600));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+        this.pack();
+        this.setVisible(true);
+
+        conteneur = null;
+    }
+
+
+    public void ajouterConteneur(JPanel conteneur){
+        this.conteneur = conteneur;
+        this.setContentPane(conteneur);
+    }
+
+
 }
