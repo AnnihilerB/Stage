2652cd606be582a7a2bbef550e338085bd8522dc
Ali <cherifiali@outlook.fr>
2016-07-02 13:21:05 +0200

    Affichage de la vidéo à traiter.

--- src/main/java/boutons/ChargerVideo.java
+++ src/main/java/boutons/ChargerVideo.java
@@ -1,14 +1,24 @@
 package boutons;
 
 import exceptions.VideoNonSupporte;
+import org.openimaj.video.VideoDisplay;
+import org.openimaj.video.VideoPlayer;
 import org.openimaj.video.xuggle.XuggleVideo;
 import traitements.Traitement;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ChargerVideo implements ActionListener {
+
+    JPanel videoPlayer;
+    JPanel conteneur;
+
+    public ChargerVideo(JPanel c){
+        conteneur = c;
+    }
 
     public void actionPerformed(ActionEvent e){
         JFileChooser choixFichier = new JFileChooser();
@@ -16,14 +26,26 @@ public class ChargerVideo implements ActionListener {
         try{
             chargerVideo(choixFichier);
         }catch (VideoNonSupporte ex){
-
         }
     }
 
     public void chargerVideo(JFileChooser fichier) throws VideoNonSupporte{
         XuggleVideo video = new XuggleVideo(fichier.getSelectedFile());
+        VideoPlayer p = VideoPlayer.createVideoPlayer(video);
+        p.pause();
+        videoPlayer = p.getVideoPlayerPanel();
+        afficherVideo();
+
         new Traitement(video);
     }
+
+    public void afficherVideo(){
+        videoPlayer.setPreferredSize(new Dimension(1024,768));
+        conteneur.add(videoPlayer);
+
+
+    }
+
 }
 
 
