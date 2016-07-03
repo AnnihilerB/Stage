00ad774281e0ab12b3fa32023591d4ae15932cfd
Ali <ali.cherifi@etu.u-bordeaux.fr>
2016-07-03 09:27:19 +0200

    Classe barreprogression

---
+++ src/main/java/interfacegraphique/BarreProgression.java
@@ -0,0 +1,14 @@
+package interfacegraphique;
+
+import javax.swing.*;
+
+/**
+ * Created by ali on 03/07/16.
+ */
+public class BarreProgression {
+    JProgressBar barre;
+
+    public BarreProgression(){
+        barre = new JProgressBar();
+    }
+}
