1d9c4bad25cf4525cc39b42bd4d113418ad961b3
Ali <acherifi@etu.u-bordeaux.fr>
2016-07-04 10:31:05 +0200

    Ajout de l'audio.

--- src/main/java/boutons/ChargerVideo.java
+++ src/main/java/boutons/ChargerVideo.java
@@ -8,6 +8,7 @@ import traitements.Traitement;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import org.openimaj.video.xuggle.XuggleAudio;
 
 public class ChargerVideo implements ActionListener {
 
@@ -31,9 +32,10 @@ public class ChargerVideo implements ActionListener {
     public void chargerVideo(JFileChooser fichier) throws VideoNonSupporte{
         //Création de deux vidéos à partir du même fichier pour supprimer le lien affichage traitement.
         XuggleVideo video = new XuggleVideo(fichier.getSelectedFile());
+        XuggleAudio audio = new XuggleAudio(fichier.getSelectedFile());
         XuggleVideo videoTraiter = new XuggleVideo(fichier.getSelectedFile());
         text.setText(fichier.getSelectedFile().getAbsolutePath());
-        VideoPlayer p = VideoPlayer.createVideoPlayer(video);
+        VideoPlayer p = VideoPlayer.createVideoPlayer(video,audio);
         p.pause();
         p.showFrame();
         new Traitement(videoTraiter,conteneur);
