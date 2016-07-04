00906e1869d00822671b8e25b745bad990a414f7
Ali <acherifi@etu.u-bordeaux.fr>
2016-07-04 10:31:51 +0200

    Traitement en fonction des KeyFrames. Taille de l'image à revoir

--- src/main/java/traitements/Traitement.java
+++ src/main/java/traitements/Traitement.java
@@ -1,6 +1,5 @@
 package traitements;
 
-
 import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
@@ -23,12 +22,12 @@ public class Traitement {
     protected static File sortie;
     protected static JPanel conteneur;
 
-    public Traitement(XuggleVideo v, JPanel cont){
+    public Traitement(XuggleVideo v, JPanel cont) {
         video = v;
         conteneur = cont;
     }
 
-    public void anaglypheDubois(){
+    public void anaglypheDubois() {
 
     }
 
@@ -39,23 +38,23 @@ public class Traitement {
         int hauteur = source.getHeight();
         int largeur = source.getWidth();
 
-        MBFImage dest = new MBFImage(largeur,hauteur);
+        MBFImage dest = new MBFImage(largeur, hauteur);
 
-        for (int y = DECALAGE; y <hauteur - DECALAGE; ++y){
-            for (int x = DECALAGE; x < largeur - DECALAGE; ++x){
+        for (int y = DECALAGE; y < hauteur - DECALAGE; ++y) {
+            for (int x = DECALAGE; x < largeur - DECALAGE; ++x) {
 
                 Float[] pixels;
                 Float[] pixelsDest;
 
                 //Recupere la couleur rouge
-                pixelsDest = source.getPixel(x - DECALAGE,y);
+                pixelsDest = source.getPixel(x - DECALAGE, y);
 
-                pixels = source.getPixel(x + DECALAGE,y);
+                pixels = source.getPixel(x + DECALAGE, y);
 
                 pixelsDest[1] = pixels[1];
                 pixelsDest[2] = pixels[2];
 
-                dest.setPixel(x,y,pixelsDest);
+                dest.setPixel(x, y, pixelsDest);
 
             }
         }
@@ -75,42 +74,46 @@ public class Traitement {
 
         MBFImage frame;
         MBFImage frameResume;
-        MBFImage imgSortie = new MBFImage((int)(TAILLEBANDE * video.countFrames()) / 10,video.getCurrentFrame().getHeight() );
+        MBFImage imgSortie = new MBFImage((int) (TAILLEBANDE * video.countFrames()) / 10, video.getCurrentFrame().getHeight());
 
         //Récupération de la première frame.
         frame = video.getCurrentFrame();
 
-        while (video.hasNextFrame()){
+        while (video.hasNextFrame()) {
+            System.out.println("While");
+            if (video.nextFrameIsKeyFrame) {
+                System.out.println("KeyFrame");
+                
+                frame = video.getNextFrame();
 
-            //Modulo 10 sinon frame trop similaires.
-            if (cpt %10 == 0){
-
+                //Modulo 10 sinon frame trop similaires.
                 //Recuperation de la bande centrale.
                 frameResume = getBandeCentrale(frame);
 
                 //Dessine dans l'image de sortie, la bande centrale récupérée.
-                imgSortie.drawImage(frameResume,cptX,0);
+                imgSortie.drawImage(frameResume, cptX, 0);
                 cptX += TAILLEBANDE;
+                
+                System.out.println(cpt);
+                cpt++;
             }
             frame = video.getNextFrame();
-            cpt++;
         }
-        ImageUtilities.write(imgSortie,"png", sortie);
+        ImageUtilities.write(imgSortie, "png", sortie);
         video.close();
         t.detruire();
-        JOptionPane.showMessageDialog(conteneur, "Traitement terminé !","Résumé vidéo",JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(conteneur, "Traitement terminé !", "Résumé vidéo", JOptionPane.PLAIN_MESSAGE);
     }
 
-    public void sideBySide() throws IOException{
-
+    public void sideBySide() throws IOException {
 
         //Récupération de l'image source et création de l'image de destination contenant les deux images gauche et droite.
         MBFImage source = ImageUtilities.readMBF(new File("src/main/resources/sbs.jpg"));
-        MBFImage dest = new MBFImage((source.getWidth() - ESPACEMENT) *2,source.getHeight());
+        MBFImage dest = new MBFImage((source.getWidth() - ESPACEMENT) * 2, source.getHeight());
 
         //Création des deux images gauches et droite auquels on retire un bout corresspondant à  ce que voit l'oeil.
-        MBFImage gauche = new MBFImage(source.getWidth() - ESPACEMENT,source.getHeight());
-        MBFImage droite = new MBFImage(source.getWidth() - ESPACEMENT,source.getHeight());
+        MBFImage gauche = new MBFImage(source.getWidth() - ESPACEMENT, source.getHeight());
+        MBFImage droite = new MBFImage(source.getWidth() - ESPACEMENT, source.getHeight());
 
         //Fenetre d'affichage des images.
         DisplayUtilities.createNamedWindow("droite");
@@ -123,26 +126,25 @@ public class Traitement {
         int indexdroite = 0;
 
         //Parcours des y de l'image.
-        for (int y = 0; y< source.getHeight(); ++y){
+        for (int y = 0; y < source.getHeight(); ++y) {
             //Remplissage de l'image gauche.
-            for (int x = 0; x < parcoursX; ++x){
+            for (int x = 0; x < parcoursX; ++x) {
                 Float[] pixels;
-                pixels = source.getPixel(x,y);
-                gauche.setPixel(x,y,pixels);
+                pixels = source.getPixel(x, y);
+                gauche.setPixel(x, y, pixels);
             }
             //Remplissage de l'aimge droite. On commence à partir de l'espcaement.
-            for (int xD = ESPACEMENT; xD < source.getWidth(); ++xD){
+            for (int xD = ESPACEMENT; xD < source.getWidth(); ++xD) {
                 Float[] pixels;
-                pixels = source.getPixel(xD,y);
+                pixels = source.getPixel(xD, y);
                 //Besoin d'un index pour remplir l'imge de destination depuis 0 et non depuis l'espacement.
                 //Ananlyse si on est en fin de parcours d'un ligne de pixel.
-                if (indexdroite == droite.getWidth()){
-                    droite.setPixel(indexdroite,y,pixels);
+                if (indexdroite == droite.getWidth()) {
+                    droite.setPixel(indexdroite, y, pixels);
                     indexdroite = 0;
-                }
-                //On est pas en fin de ligne donc remplissage.
-                else{
-                    droite.setPixel(indexdroite,y,pixels);
+                } //On est pas en fin de ligne donc remplissage.
+                else {
+                    droite.setPixel(indexdroite, y, pixels);
                 }
                 indexdroite++;
 
@@ -150,8 +152,8 @@ public class Traitement {
 
         }
         //Remplissage de l'image finale à l'aide des images gauche et droite.
-        dest.drawImage(gauche,0,0);
-        dest.drawImage(droite,gauche.getWidth(),0);
+        dest.drawImage(gauche, 0, 0);
+        dest.drawImage(droite, gauche.getWidth(), 0);
 
         //Affichage.
         //DisplayUtilities.displayName(gauche,"gauche");
@@ -167,21 +169,20 @@ public class Traitement {
         //Parcours des X
         int cptXDest = 0;
 
-        MBFImage sortie = new MBFImage(TAILLEBANDE,hauteur);
+        MBFImage sortie = new MBFImage(TAILLEBANDE, hauteur);
 
-        for (int y = 0; y < hauteur; ++y){
-            for (int x = milieu - (TAILLEBANDE / 2); x < milieu + (TAILLEBANDE / 2); ++x ){
-                sortie.setPixel(cptXDest,y,source.getPixel(x,y));
-                cptXDest ++;
+        for (int y = 0; y < hauteur; ++y) {
+            for (int x = milieu - (TAILLEBANDE / 2); x < milieu + (TAILLEBANDE / 2); ++x) {
+                sortie.setPixel(cptXDest, y, source.getPixel(x, y));
+                cptXDest++;
             }
             cptXDest = 0;
         }
         return sortie;
     }
 
-    public static void setFile(File f){
+    public static void setFile(File f) {
         sortie = f;
     }
 
 }
-
