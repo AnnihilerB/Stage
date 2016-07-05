8d22eb08dc0c343cae7fabb046c8e4443bd65e4a
Ali <ali.cherifi@etu.u-bordeaux.fr>
2016-07-05 17:58:00 +0200

    Listener pour lancer l'anaglyphe.

---
+++ src/main/java/boutons/TraitementAnaglyphe.java
@@ -0,0 +1,14 @@
+package boutons;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by ali on 04/07/16.
+ */
+public class TraitementAnaglyphe implements ActionListener{
+
+    public void actionPerformed(ActionEvent actionEvent) {
+        //Traitement.anaglyphe();
+    }
+}
