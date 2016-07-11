bcf2d2ebb5ddfe5dbe67f098a2bbfa6617c68f8e
Ali <ali.cherifi@etu.u-bordeaux.fr>
2016-07-11 10:10:41 +0200

    Premiere implémentations de la claasse JUnit

---
+++ src/main/java/TraitementsTest.java
@@ -0,0 +1,13 @@
+import org.apache.xmlbeans.impl.tool.XSTCTester;
+
+/**
+ * Created by Ali on 11/07/2016.
+ */
+public class TraitementsTest extends XSTCTester.TestCase {
+
+    public void testBandeCentrale(){
+
+    }
+
+
+}
