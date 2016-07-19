4751cac6cbb311bb56c9e3fab9099e1ef4bfd933
Ali <ali.cherifi@etu.u-bordeaux.fr>
2016-07-19 09:54:52 +0200

    Classe permettant l'obtention des options pour le barcode.

--- src/main/java/utils/OptionsBarcode.java
+++ src/main/java/utils/OptionsBarcode.java
@@ -1,77 +1,31 @@
 package utils;
 
-import boutons.TraitementBarcode;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 /**
- * Created by ali on 04/07/16.
+ * Created by Ali on 18/07/2016.
  */
-public class OptionsBarcode implements ActionListener {
+public class OptionsBarcode {
 
-    static JDialog options;
-    int hauteur = 1280;
-    int tailleBande = 5;
+    int largeur;
+    int tailleBande;
 
-    public void actionPerformed(ActionEvent actionEvent) {
-        this.ouvrirFenetreOptions();
+    public OptionsBarcode(){
+        largeur = 1080;
+        tailleBande = 5;
     }
 
-    /**
-     * Méthode affichant une fenête des options permettant de configurer le barcode
-     */
-
-    private void ouvrirFenetreOptions(){
-        // Choix de la largeur de l'image, taille de la bande, calcul de la taille de l'image.
-        options = new JDialog();
-
-        //Paramètrage des textes.
-        JLabel labLargeur = new JLabel("Largeur de l'image : ");
-        JLabel labTailleBande = new JLabel("Taille de la bande : ");
-
-        //Zones de texte où rentrer les valeurs.
-        JTextArea textLargeur = new JTextArea(1,4);
-        JTextArea textTailleBande = new JTextArea(1,4);
-
-        textLargeur.setAutoscrolls(false);
-
-        //Bouton pour valider les valeurs entrées.
-        JButton lancerTraitement = new JButton("Valider");
-        JButton validerHauteur = new JButton("Ok");
-        JButton validerTaille = new JButton("Ok");
-
-        //Paramètrage de la fenetre.
-        options.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-        options.setSize(300,200);
-        options.setLayout(new FlowLayout(FlowLayout.LEFT,15,25));
-        options.setTitle("Paramètres de sortie");
-
-        //Ajout des éléments dans la fenetre.
-        options.add(labLargeur);
-        options.add(textLargeur);
-        options.add(validerHauteur);
-        options.add(labTailleBande);
-        options.add(textTailleBande);
-        options.add(validerTaille);
-        options.add(lancerTraitement);
-
-        //Ajout des Listener sur les boutons.
-        lancerTraitement.addActionListener(new TraitementBarcode());
-
-        //Affichage de la fenetre.
-        options.setVisible(true);
-
-        System.out.print(textLargeur.getText());
-
+    public int getLargeur() {
+        return largeur;
     }
 
-    /**
-     * Destruction de la fenetre des options.
-     */
-    public static void detruireOptions(){
-        options.dispose();
+    public void setLargeur(int l) {
+        largeur = l;
+    }
+
+    public int getTailleBande() {
+        return tailleBande;
+    }
+
+    public void setTailleBande(int tBande) {
+        tailleBande = tBande;
     }
 }
