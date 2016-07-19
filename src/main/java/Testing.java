e1f723771282822905a464d3a791e9b97101b060
Ali <ali.cherifi@etu.u-bordeaux.fr>
2016-07-19 09:55:30 +0200

    Testing

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -1,10 +1,14 @@
+import traitements.Traitement;
+
 import java.io.IOException;
 
 public class Testing {
 
     public static void main(String args[]) throws IOException {
 
-       // Traitement.anaglypheDubois();
+        Traitement.decouperImageGauche();
+        Traitement.decouperImageDroite();
+        Traitement.anaglypheDubois();
 
 
 
