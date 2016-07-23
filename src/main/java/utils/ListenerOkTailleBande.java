c45eeede9d9f24f81117e67885f1eb12903601d9
Ali <ali.cherifi@etu.u-bordeaux.fr>
2016-07-23 10:46:40 +0200

    Listener pour les options barcode Taille Bande

---
+++ src/main/java/utils/ListenerOkTailleBande.java
@@ -0,0 +1,24 @@
+package utils;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by ali on 23/07/16.
+ */
+public class ListenerOkTailleBande implements ActionListener {
+
+    JTextArea textTailleBande;
+
+    public ListenerOkTailleBande(JTextArea text){
+        textTailleBande = text;
+
+    }
+
+    public void actionPerformed(ActionEvent actionEvent) {
+        OptionsBarcode.setTailleBande(Integer.parseInt(textTailleBande.getText()));
+        System.out.println(OptionsBarcode.getTailleBande());
+
+    }
+}
