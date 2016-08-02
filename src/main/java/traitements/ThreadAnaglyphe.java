372008d6f426a43ec594aa611d04b904a35e16e2
Ali <ali.cherifi@etu.u-bordeaux.fr>
2016-08-02 13:02:42 +0200

    Ajour stacktrace

--- src/main/java/traitements/ThreadAnaglyphe.java
+++ src/main/java/traitements/ThreadAnaglyphe.java
@@ -14,7 +14,7 @@ public class ThreadAnaglyphe extends Thread {
         } catch (IOException e) {
             e.printStackTrace();
         } catch (DestinationManquante destinationManquante) {
-
+            destinationManquante.printStackTrace();
         }
     }
 
